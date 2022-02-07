f08d94d2fea4ac26e1ff50833bf5b837d8acf26c
fanaczan <fanaczan@vasvari.org>
2022-02-07 08:43:51 +0100

    Dependencies,repositories hozzáadva.

---
+++ app/src/main/java/com/example/orderpizza/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.orderpizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
